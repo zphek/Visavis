04d7d0dd694c73c90fa6c09b2470a9611d493340
bernardobaez <bernardbaez.beno@gmail.com>
2024-04-27 21:28:47 -0400

    ......

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommunicationGateway } from './communication/communication.gateway';
+import { ServerGateway } from './server/server.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CommunicationGateway, ServerGateway],
 })
 export class AppModule {}
